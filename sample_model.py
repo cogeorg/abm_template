d6ca9da888416a7f5f2ac19141a6155b519a3ac5
pafiedor <pawel@fiedor.eu>
2015-11-20 13:36:50 +0200

    Issue #1
    Added a simple game as a working example

--- sample_model.py
+++ sample_model.py
@@ -1,12 +1,14 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
+# [SublimeLinter pep8-max-line-length:300]
 # -*- coding: utf-8 -*-
 
 import math
+import random
 
-from basemodel import BaseModel
+from src.basemodel import BaseModel
 
-from config import Config
-from agent import Agent
+from sample_config import Config
+from sample_agent import Agent
 
 
 class Model(BaseModel):
@@ -23,6 +25,7 @@ class Model(BaseModel):
 
     def get_identifier(self):
         return self.identifier
+
     def set_identifier(self, _value):
         """
         Class variables: identifier
@@ -32,6 +35,7 @@ class Model(BaseModel):
 
     def get_model_parameters(self):
         return self.model_parameters
+
     def set_model_parameters(self, _value):
         """
         Class variables: model_parameters
@@ -41,6 +45,7 @@ class Model(BaseModel):
 
     def get_agents(self):
         return self.agents
+
     def set_agents(self, _value):
         """
         Class variables: agents
@@ -50,6 +55,7 @@ class Model(BaseModel):
 
     def get_interactions(self):
         return self.interactions
+
     def set_interactions(self, _value):
         """
         Class variables: interactions
@@ -60,9 +66,10 @@ class Model(BaseModel):
     def __str__(self):
         return super(Model, self).__str__()
 
-
     def initialize_agents(self):
-        super(Model, self).initialize_agents()
+        for agent_iterator in range(0, int(self.model_parameters['num_agents'])):
+            temp_agent = Agent(str(agent_iterator), {}, {})
+            self.agents.append(temp_agent)
 
     def get_agent_by_id(self, _id):
         """
@@ -92,76 +99,66 @@ class Model(BaseModel):
                 temp_state_variable == agent_iterator.state_variables[parameter_iterator]
         return True
 
-    # TODO this code is fairly general and should be generalized further and then moved to the BaseAgent class
-    def compute_equilibrium_recursive(self, agentA, agentB):
-        """
-        Class variables: par_keys, par_lower, par_upper, par_current, par_step, steps_per_state_variable, precision
-        Local variables: agentA, agentB, state_key, state_var
-        """
-        self.par_keys = []
-        self.par_lower = []
-        self.par_upper = []
-        self.par_step  = []
-        self.par_current = []
-        for state_key, state_var in agentA.state_variables:
-            self.par_keys.append(str(state_key))
-            self.par_lower.append(float(state_var[0]))
-            self.par_upper.append(float(state_var[1]))
-            self.par_step.append((self.par_upper[-1] - self.par_lower[-1]) / self.steps_per_state_variable)
-        
-        self.precision = 0.01
-        self.par_current = self.par_lower
-        self.loop_over_dimension(0, agentA, agentB)
-
-    # TODO this code is fairly general and should be generalized further and then moved to the BaseAgent class
-    def loop_over_dimension(self, recursion_level, agentA, agentB):
-        """
-        Class variables: par_current, par_upper, par_lower
-        Local variables: agentA, agentB, recursion_level
-        """
-        while self.par_current[recursion_level] <= self.par_upper[recursion_level]:
-            if (recursion_level+1) < len(self.par_upper):
-                self.par_current[recursion_level+1] = self.par_lower[recursion_level+1]
-                self.loop_over_dimension(recursion_level+1, agentA, agentB)
-            elif (recursion_level+1) == len(self.par_upper):
-                self.check_fixed_point(agentA, agentB)
-            self.par_current[recursion_level] += self.par_step[recursion_level]
 
     # TODO this code is fairly general and should be generalized further and then moved to the BaseAgent class
     def check_fixed_point(self, agentA, agentB):
-        """
-        Class variables: par_keys, par_current
-        Local variables: agentA, agentB, state_iterator, ret_B, ret_A, i, x, y
-        """
-        for state_iterator in range(0,len(self.par_keys)):
-                    agentA.state_variables[self.par_keys[state_iterator]] = self.par_current[state_iterator]
 
-                # then get the best response of B given the current portfolio choice of A
-                ret_B = agentB.get_best_response(self.par_current)
+        actions = [0, 1]
+        agentA.parameters['action'] = random.randint(min(actions), max(actions))
+        agentB.parameters['action'] = random.randint(min(actions), max(actions))
+        for iterator in range(0, int(self.model_parameters['num_sweeps'])):
+            originalactionA = agentA.parameters['action']
+            originalactionB = agentB.parameters['action']
+            # then get the best response of B given the current portfolio choice of A
+            print self.get_best_response(agentA.parameters['action'])
+            agentB.parameters['action'] = self.get_best_response(agentA.parameters['action'])
 
-                # and then get the best response of A given the best response of B
-                ret_A = agentA.get_best_response(ret_B)
+            # and then get the best response of A given the best response of B
+            print self.get_best_response(agentB.parameters['action'])
+            agentA.parameters['action'] = self.get_best_response(agentB.parameters['action'])
 
-                # check if we have a fixed point
-                if all(i < self.precision for i in [abs(x) - y for x, y in zip(ret_A, self.par_current)]):
-                    # here we have to write out the results
-                    pass
+            # check if we have a fixed point
+            if (iterator != 0 and originalactionA == agentA.parameters['action'] and originalactionB == agentB.parameters['action']):
+                # here we have to write out the results
+                print "Found equilibrium!"
+                self.model_parameters['equilibrium'] = "yes"
+                return
+        print "No equilibrium found!"
+        self.model_parameters['equilibrium'] = "no"
+
+    def get_best_response(self, reaction):
+        if self.payout_from_game(1, reaction) > self.payout_from_game(0, reaction):
+            return 1
+        else:
+            return 0
 
     def do_update(self):
-        """
-        Class variables: agents, steps_per_state_variable, model_parameters
-        Local variables: agentA, agentB
-        """
         # equilibrium is found by iterating over all possible variable choices for agent A, communicating them to
         # agent B, obtaining B's best response by iterating over all possible variable choices for B, communicating
         # B's best response back to A, and computing A's best response. Any fixed point of this procedure is an
         # equilibrium
         agentA = self.agents[0]
         agentB = self.agents[1]
+        self.check_fixed_point(agentA, agentB)
+        if self.model_parameters['equilibrium'] == "no":
+            pass
+        else:
+            return [agentA.parameters['action'], agentB.parameters['action']]
 
-        self.steps_per_state_variable = math.pow(float(self.model_parameters['num_sweeps']),1.0/len(agentA.state_variables))
-        self.compute_equilibrium_recursive(agentA, agentB)
-
-        # we need the stepwidth
-        #print self.model_parameters
-        #print agentA.state_variables.keys(), agentA.state_variables
+    def payout_from_game(self, action, reaction):
+        if action == 1:
+            if reaction == 1:
+                return 1-float(self.model_parameters['theta'])
+            elif reaction == 0:
+                return -float(self.model_parameters['theta'])
+            else:
+                print "Wrong action, check the code!"
+        elif action == 0:
+            if reaction == 1:
+                return 0
+            elif reaction == 0:
+                return 0
+            else:
+                print "Wrong action, check the code!"
+        else:
+            print "Wrong action, check the code!"
