08b0f1485eeb84d3d7302fbf37dc00036db6483d
pafiedor <pawel@fiedor.eu>
2016-04-29 12:23:42 +0200

    Base Shock class

---
+++ src/baseshock.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import abc
+
+__author__ = """Paweł Fiedor (pawel.fiedor@uct.ac.za)"""
+
+# -------------------------------------------------------------------------
+#
+#  class Shock
+#
+# -------------------------------------------------------------------------
+
+
+class BaseShock(object):
+
+    @abc.abstractmethod
+    def do_shock(self, environment, time):
+        pass
